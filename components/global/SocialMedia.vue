b97108e16037f75bc867ad426b58a33914bacb77
Andreas Blanke <blan@itu.dk>
2021-01-27 12:58:18 +0100

    added lost files from previous commit

---
+++ components/global/SocialMedia.vue
@@ -0,0 +1,23 @@
+<template>
+  <em v-if="type === 'linkedin'">
+    <font-awesome-icon :icon="['fab', 'linkedin']" />
+    <a target="_blank" href="https://www.linkedin.com/in/andreas-b-5a926b84">Connect with me on LinkedIn</a>
+  </em>
+  <em v-else-if="type === 'github'">
+    <font-awesome-icon :icon="['fab', 'github']" />
+    <a target="_blank" href="https://github.com/andSubmarine">See my public Github profile</a>
+  </em>
+</template>
+
+<script lang="ts">
+import { Prop, Component, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class SocialMedia extends Vue {
+    @Prop({ required: true }) type!: string;
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/articles/_id/index.vue
@@ -0,0 +1,88 @@
+<template>
+  <b-container v-if="page" tag="article">
+    <ToTopButton />
+    <BackButton route="/" />
+    <div class="row">
+      <b-img v-if="page.url" id="article-img" :src="page.url" fluid :alt="page.alt" />
+      <div id="article-body">
+        <h1>{{ page.title }}</h1>
+        <em id="description">{{ page.description }}</em>
+        <nuxt-content id="article-content" :document="page" />
+        <hr>
+        <b-media>
+          <template #aside>
+            <b-img src="/blanke-tech/blob/profile.jpg" blank-color="#ccc" width="64" alt="author profile" />
+          </template>
+          <p v-if="lastUpdated" class="mb-1"> <em>{{ lastUpdated }}</em> </p>
+          <p class="mb-1"> <em>Written by Andreas Blanke</em> </p>
+          <p class="mb-1"> <SocialMedia type="linkedin" /> </p>
+          <p class="mb-1"> <SocialMedia type="github" /> </p>
+        </b-media>
+      </div>
+    </div>
+  </b-container>
+</template>
+
+<script lang="ts">
+import { Prop, Component, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class Article extends Vue {
+    page: any = {};
+
+    head () {
+      return {
+        title: this.page.title + ' | ' + process.env.title,
+        meta: [
+          {
+            hid: this.page.slug
+          }
+        ]
+      }
+    }
+
+    async created () {
+      const id = this.$route.params.id
+      if (id) {
+        this.page = await this.$content('articles', id).fetch()
+      } else {
+        this.$router.push('/')
+      }
+    }
+
+    get lastUpdated () {
+      if (this.page) {
+        return 'Last updated: ' + new Date(this.page.updatedAt).toLocaleString('en-GB', {
+          year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit'
+        })
+      } else { return undefined }
+    }
+}
+</script>
+
+<style lang="scss">
+article {
+  background-color: #F2EEEB;
+  min-height: 100vh;
+}
+#article-body {
+  max-width: 100%;
+  padding: 1rem 2rem;
+  padding-bottom: 5rem;
+}
+#description {
+  font-size: 1.2rem;
+  margin: 2rem 0;
+}
+#article-content {
+  margin-top: 1rem;
+}
+.img-fluid {
+  width: 100%;
+}
+#article-img {
+  max-height: 50vh;
+  object-fit: cover;
+  object-position: 0 0;
+}
+</style>
