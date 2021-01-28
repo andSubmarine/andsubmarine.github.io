6b02a98ac69f1de6e1dff1d8e63af271ee9ef6bb
Andreas Blanke <blan@itu.dk>
2021-01-28 15:01:39 +0100

    enabled hash routing fallback and added CSP for loading favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,6 +8,7 @@ export default {
    * See https://nuxtjs.org/api/configuration-target
    */
   target: 'static',
+  fallback: true,
   /* Headers of the page
    * See https://nuxtjs.org/api/configuration-head
    */
@@ -16,7 +17,8 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' },
+      { http: 'Content-Security-Policy', content: "img-src * 'self' data: https:" }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: './favicon.ico' }
