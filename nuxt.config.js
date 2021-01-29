33dfdc483571b38839da62c158cc73c2d70ccf85
Andreas Blanke <blan@itu.dk>
2021-01-29 14:46:22 +0100

    switches target from static to server to try and fix reload issue

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
   /* Nuxt target
    * See https://nuxtjs.org/api/configuration-target
    */
-  target: 'static',
+  target: 'server',
   fallback: true,
   /* Headers of the page
    * See https://nuxtjs.org/api/configuration-head
